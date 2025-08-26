340acf0c3fe4a54f2a2cc644b53b2b4a7c01eb91
apkohl-kt <akohl@msudenver.edu>
2025-08-26 17:29:25 -0600

    Update word_recombiner.js

--- word_recombiner.js
+++ word_recombiner.js
@@ -1,21 +1,38 @@
 // unfinished draft of the JS file, written in JDScript for prototyping
 
-var totalwordcount = 2 ^ "word".length;
+word = document.getElementById("form1").value;
+wordpermutations = factorial(word)
 
-function makenewword(word, recombinations) {
-  for (let i of word) {
+function factorial(n) {
+    let res = 1;
+    for (let i = 1; i <= n; i++) { res *= i; }
+    return res;
+}
+
+document.getElementById("output").innerHTML = wordpermutations;
+
+
+
+
+
+//function makenewword(word, recombinations) {
+  //for (let i of word) {
     //...
-  }
+  //}
+  //var tempword = word;
+  //for (let i of word) {
+    //tempword = tempword.slice(1); // pop first letter
+    //var newword = i;
+  //}  
+//}
 
-  var tempword = word;
-  for (let i of word) {
-    tempword = tempword.slice(1); // pop off first letter
-    var newword = i;
-  }
-}
-
-function _ready() {
-  var word = "hello"; // change this to be the input from html
-  var recombinations = new Array(totalwordcount);
-  makenewword(word, recombinations);
-}
+//function main() {
+  //var word = "hello"; // change this to be the input from html
+  //var totalwordcount = factorial(word.length);
+  //console.log(totalwordcount);
+  //console.log("Try programiz.pro");
+  //var recombinations = new Array(totalwordcount);
+  //for (let i of totalwordcount) {
+    //makenewword(word, recombinations);
+  //}
+//}
